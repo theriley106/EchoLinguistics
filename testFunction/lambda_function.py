65e3b6aa2b5025dd7765dfb70619d076b5ee4800
Theriley106 <christopherlambert106@gmail.com>
2018-02-09 06:10:34 -0500

    Used generate text function instead of rewriting lines 104 & 105 manually

--- testFunction/lambda_function.py
+++ testFunction/lambda_function.py
@@ -101,7 +101,7 @@ def on_intent(intent_request, session):
 		languageName = returnLanguageSlotValue(intent)
 		languageAbbreviation = returnLanguageAbbrFromFull(languageName)
 		# this should be a lower case abbreviation: ie. es or en | languageAbbreviation is also accent for this intent
-		text = translateText(TEXT_TO_SAY.format(languageName, languageName), toLanguage=languageAbbreviation)
+		text = generateText(languageName, languageName, languageAbbreviation)
 		#This generates the text that the alexa says - it will translate from the english in TEXT_TO_SAY
 		if checkInFile(region) == False:
 			f = {
