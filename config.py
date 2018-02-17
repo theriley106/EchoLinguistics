3cd3dc83f7d5b1e511f9434fd97d735ac086bb3e
Theriley106 <christopherlambert106@gmail.com>
2018-02-17 17:03:39 -0500

    Added config.py successfully

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 continueVal = True
-if os.file.exists("accessKey.txt") == True or os.file.exists("secretKey.txt") == True:
+if os.path.exists("accessKey.txt") == True or os.path.exists("secretKey.txt") == True:
 	if raw_input("Files already exist.  Overwrite now? (y/n) ").lower() == 'n':
 		continueVal = False
 		accessKey = open("accessKey.txt").read().strip()
 		secretKey = open("secretKey.txt").read().strip()
-if continueVal = True:
+if continueVal == True:
 	accessKey = raw_input("AWS Access Key: ")
 	secretKey = raw_input("AWS Secret Key: ")
 	print("You need to create an S3 Bucket with PUBLIC permissions before using this program")
@@ -14,12 +14,22 @@ if continueVal = True:
 	bucketID = raw_input("S3 Bucket ID: ")
 	if bucketID[-1] != '/':
 		bucketID = bucketID + '/'
+	file = open("accessKey.txt", "w")
+	file.write(accessKey)
+	file.close()
 
+	file = open("secretKey.txt", "w")
+	file.write(secretKey)
+	file.close()
+
+	file = open("bucketID.txt", "w")
+	file.write(bucketID)
+	file.close()
 
 
 def uploadFile(fileName):
-	bucketIDVal = bucketID.partition(".com/")[2].partition("/")[0]
-	conn = tinys3.Connection(accessKey,secretKey,tls=True)
+	bucketIDVal = open("bucketID.txt").read().strip().partition(".com/")[2].partition("/")[0]
+	conn = tinys3.Connection(open("accessKey.txt").read().strip(),open("secretKey.txt").read().strip(),tls=True)
 	conn.upload(fileName, open(fileName,'rb'), bucketIDVal)
 	return "https://s3.amazonaws.com/{}/{}".format(bucketIDVal, fileName)
 
@@ -32,16 +42,11 @@ if raw_input("Successfully grabbed configuration keys.  Test now? (y/n) ").lower
 	os.system("echo 'Echo Linguistics Test Upload' > testUpload.txt")
 	fileUpload = uploadFile('testUpload.txt')
 	print("Successfully uploaded file to {}".format(fileUpload))
+	os.remove("testUpload.txt")
 
 
 
-file = open("accessKey.txt", "w")
-file.write(accessKey)
-file.close()
 
-file = open("secretKey.txt", "w")
-file.write(secretKey)
-file.close()
 
 
 
