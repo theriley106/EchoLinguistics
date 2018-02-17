ef7133e8f0436c99b87c60bf0e9b60d4b777d5fa
Theriley106 <christopherlambert106@gmail.com>
2018-02-17 16:42:42 -0500

    Added bucket ID

--- config.py
+++ config.py
@@ -1,9 +1,23 @@
 import os
 accessKey = raw_input("AWS Access Key: ")
 secretKey = raw_input("AWS Secret Key: ")
+print("You need to create an S3 Bucket with PUBLIC permissions before using this program")
+print("\nBucket ID Example: https://s3.amazonaws.com/bucketName/")
+bucketID = raw_input("S3 Bucket ID: ")
+if bucketID[-1] != '/':
+	bucketID = bucketID + '/'
+
+
+
+
 file = open("accessKey.txt", "w")
 file.write(accessKey)
 file.close()
+
 file = open("secretKey.txt", "w")
 file.write(secretKey)
 file.close()
+
+
+
+if raw_input("You need to create a ")
