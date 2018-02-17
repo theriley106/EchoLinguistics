9d94df20c716460c95d9b97b8c628023782ebea5
Theriley106 <christopherlambert106@gmail.com>
2018-02-17 16:39:07 -0500

    Added the secret key and access key creation

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
-
 accessKey = raw_input("AWS Access Key: ")
 secretKey = raw_input("AWS Secret Key: ")
+file = open("accessKey.txt", "w")
+file.write(accessKey)
+file.close()
+file = open("secretKey.txt", "w")
+file.write(secretKey)
+file.close()
