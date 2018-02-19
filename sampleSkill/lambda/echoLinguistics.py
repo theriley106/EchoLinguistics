93e600f419ff28b34a0eb28661f7a0ea2a43a2ee
Theriley106 <christopherlambert106@gmail.com>
2018-02-19 08:02:36 -0500

    Added transition to modularized echo linguisitcs program

--- sampleSkill/lambda/echoLinguistics.py
+++ sampleSkill/lambda/echoLinguistics.py
@@ -1,1 +1,19 @@
 from googletrans import Translator
+
+
+try:
+	SECRET_KEY = open("secretKey.txt").read().strip()
+	ACCESS_KEY = open("accessKey.txt").read().strip()
+	tempBucketID = open("bucketID.txt").read().strip()
+except:
+	print("No security credentials set up")
+	print("create secretCode.txt and accessKey.txt")
+	raise Exception("No config files")
+
+# This just confirms that you have all configuration files
+FFMPEG_FILE_LOCATION = "/tmp/ffmpeg.linux64"
+# /tmp/ is the only lambda folder you have r/w access to
+shutil.copyfile('/var/task/ffmpeg.linux64', FFMPEG_FILE_LOCATION)
+# This copies the files in /var/task/ffmpeg.linux64 on every lambda run
+os.chmod(FFMPEG_FILE_LOCATION, os.stat(FFMPEG_FILE_LOCATION).st_mode | stat.S_IEXEC)
+# This makes that ffmpeg file executable
--- sampleSkill/lambda/lambda_function.py
+++ sampleSkill/lambda/lambda_function.py
@@ -11,24 +11,8 @@ import json
 import tinys3
 import time
 import stat
-#import botoClient
 
-try:
-	SECRET_KEY = open("secretKey.txt").read().strip()
-	ACCESS_KEY = open("accessKey.txt").read().strip()
-	tempBucketID = open("bucketID.txt").read().strip()
-except:
-	print("No security credentials set up")
-	print("create secretCode.txt and accessKey.txt")
-	raise Exception("No config files")
 
-# This just confirms that you have all configuration files
-FFMPEG_FILE_LOCATION = "/tmp/ffmpeg.linux64"
-# /tmp/ is the only lambda folder you have r/w access to
-shutil.copyfile('/var/task/ffmpeg.linux64', FFMPEG_FILE_LOCATION)
-# This copies the files in /var/task/ffmpeg.linux64 on every lambda run
-os.chmod(FFMPEG_FILE_LOCATION, os.stat(FFMPEG_FILE_LOCATION).st_mode | stat.S_IEXEC)
-# This makes that ffmpeg file executable
 SKILL_NAME = "Echo Linguistics"
 # This is also the card title
 GREETING_MESSAGE = "Modifying Amazon Echo Speech using speech synthesis markup language by Christopher Lambert"
