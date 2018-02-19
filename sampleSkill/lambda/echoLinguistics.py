03cefbab3151d6f8bb6f94fd7bde6b9f7e318896
Theriley106 <christopherlambert106@gmail.com>
2018-02-19 08:03:59 -0500

    Moved get accent to it's own program

--- sampleSkill/lambda/echoLinguistics.py
+++ sampleSkill/lambda/echoLinguistics.py
@@ -17,3 +17,29 @@ shutil.copyfile('/var/task/ffmpeg.linux64', FFMPEG_FILE_LOCATION)
 # This copies the files in /var/task/ffmpeg.linux64 on every lambda run
 os.chmod(FFMPEG_FILE_LOCATION, os.stat(FFMPEG_FILE_LOCATION).st_mode | stat.S_IEXEC)
 # This makes that ffmpeg file executable
+SSML_URL = tempBucketID + "{0}"
+# This is the url of the S3 Bucket - make sure this is a publicly available bucket.
+DB_FILE = '/tmp/mp3List.txt'
+# This is the file where the previously generated file information is stored
+LOW_BANDWIDTH = True
+# This tells the skill whether or not to regrab duplicate files
+LANGUAGE_LIST = json.loads(open("supportedLanguages.json").read())
+# This contains all supported languages
+
+def genAccentSSML(intent):
+	languageName = returnLanguageSlotValue(intent, default="English")
+	# Full name of the language sent in the request: ie, English, Spanish, etc.
+	languageAbbreviation = returnLanguageAbbrFromFull(languageName)
+	#Defines the abbreviated version of the language sent in the request
+	accentVal = intent['slots']['accentVal']['value']
+	# defines the accent language
+	accentAbbreviation = returnLanguageAbbrFromFull(accentVal)
+	# returns accent abbreviation
+	text = generateText(languageName, accentVal, languageAbbreviation)
+	# generate text that gets returned
+	print("tell me something in {} in a {} accent".format(languageName, accentVal))
+	#purely for debug reasons
+	generateSSML(text, accentAbbreviation)
+	# This is the function that generates the ssml audio object
+	return returnSSMLResponse("{}.mp3".format(accentAbbreviation))
+	# This is the python dict that the echo can interperet
